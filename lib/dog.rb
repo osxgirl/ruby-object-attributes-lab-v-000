b16f68ba8634d5b5ef6b1eafbe71680ac6d0ecb8
Heather Marie Haas <osxgirl@mac.com>
2017-11-21 19:17:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,20 @@ class Dog
     @breed = breed
   end
 
+  def name=(new_name)
+    @name = name
+  end
+
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = name
+  def breed=(breed)
+    @breed = breed
   end
+
+  def breed
+    @breed
+  end
+    
 end
